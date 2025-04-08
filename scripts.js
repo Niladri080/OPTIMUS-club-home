69f87a9b58678e9138672a3f404486fcfa49db57
Niladri080 <nilmandal080@gmail.com>
2025-04-08 11:55:25 +0530

    Initial commit - Optimus homepage

---
+++ scripts.js
@@ -0,0 +1,20 @@
+AOS.init();
+  const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
